f46e0016abd21004f1c5aa89ce302a13bfa0e581
Simon Martinelli <simon@72.services>
2018-06-21 14:51:36 +0200

    added entity and postgres

--- src/main/java/io/seventytwo/openshift/demo/DemoEntity.java
+++ src/main/java/io/seventytwo/openshift/demo/DemoEntity.java
@@ -1,4 +1,28 @@
 package io.seventytwo.openshift.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class DemoEntity {
+
+    @Id
+    private Integer id;
+    private Integer name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
 }
